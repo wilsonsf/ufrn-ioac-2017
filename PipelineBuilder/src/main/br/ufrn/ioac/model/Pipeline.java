0905500130b10e7f845d2274b383b5f9f4c99022
Wilson Farias <wilsonsf1@gmail.com>
2017-04-27 11:43:24 -0300

    Adicionadas classe e Teste para Pipeline

---
+++ PipelineBuilder/src/main/br/ufrn/ioac/model/Pipeline.java
@@ -0,0 +1,5 @@
+package br.ufrn.ioac.model;
+
+public class Pipeline {
+
+}
---
+++ PipelineBuilder/src/main/br/ufrn/ioac/model/PipelineTest.java
@@ -0,0 +1,14 @@
+package br.ufrn.ioac.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PipelineTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
