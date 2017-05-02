f8fa51fd162440c2f2b03192b449015d666b1e28
Wilson Farias <wilsonsf1@gmail.com>
2017-05-02 00:21:51 -0300

    Added Instruction pipelining test and implementation.

--- PipelineBuilder/src/main/br/ufrn/ioac/model/Pipeline.java
+++ PipelineBuilder/src/main/br/ufrn/ioac/model/Pipeline.java
@@ -1,5 +1,31 @@
 package br.ufrn.ioac.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Pipeline {
+
+	private List<Instruction> instructions = new LinkedList<Instruction>();
+	private Instruction instruction;
+
+	public void add(Instruction instruction) {
+		instructions.add(instruction);
+	}
+
+	public String logState() {
+		return "";
+	}
+
+	public Instruction getInstruction() {
+		return instruction;
+	}
+
+	public Integer countInstructions() {
+		return instructions.size();
+	}
+
+	public Instruction getCurrentInstruction() {
+		return instructions.isEmpty() ? null : instructions.get(0);
+	}
 
 }
--- PipelineBuilder/src/test/br/ufrn/ioac/model/PipelineTest.java
+++ PipelineBuilder/src/test/br/ufrn/ioac/model/PipelineTest.java
@@ -1,14 +1,35 @@
 package br.ufrn.ioac.model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PipelineTest {
 
+	private Pipeline pipeline;
+
+	@Before
+	public void before(){
+		pipeline = new Pipeline();
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void shouldAcceptAnInstruction(){
+		pipeline.add(addInstructionBuilder());
+		assertEquals(pipeline.countInstructions(), Integer.valueOf(1));
+		assertEquals("add $s0, $s1, $s2", pipeline.getCurrentInstruction().toString());
+	}
+
+	@Test
+	public void shouldAcceptManyInstructions(){
+		pipeline.add(addInstructionBuilder());
+		pipeline.add(addInstructionBuilder());
+		assertEquals(pipeline.countInstructions(), Integer.valueOf(2));
+	}
+
+	private Instruction addInstructionBuilder() {
+		return new Instruction("add","$s0","$s1","$s2");
 	}
 
 }
