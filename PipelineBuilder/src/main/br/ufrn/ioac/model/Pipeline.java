05c4235de4967f948e7851335cd94e7e2b9146db
Wilson Farias <wilsonsf1@gmail.com>
2017-05-04 23:08:10 -0300

    Added support for 2 instruction for pipeline. (test and dev)

--- PipelineBuilder/src/main/br/ufrn/ioac/model/Pipeline.java
+++ PipelineBuilder/src/main/br/ufrn/ioac/model/Pipeline.java
@@ -1,19 +1,48 @@
 package br.ufrn.ioac.model;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 public class Pipeline {
 
-	private List<Instruction> instructions = new LinkedList<Instruction>();
+	private static final int INSTRUCTION_FETCH_INDEX = 0;
+	private static final int INSTRUCTION_DECODE_INDEX = 1;
+	private static final int EXECUTION_INDEX = 2;
+	private static final int MEMORY_INDEX = 3;
+	private static final int WRITEBACK_INDEX = 4;
+
+	private ArrayList<Instruction> originalInstructions = new ArrayList<Instruction>();
+	private LinkedList<Instruction> pipeline = new LinkedList<Instruction>();
 	private Instruction instruction;
 
+	private Integer currentInstructionIndex = 0;
+
 	public void add(Instruction instruction) {
-		instructions.add(instruction);
+		originalInstructions.add(instruction);
 	}
 
 	public String logState() {
-		return "";
+		return "IF:\t" + getIFLog() + "\n" +
+				"ID:\t" + getIDLog() + "\n" +
+				"EX:\t" + "0\n" +
+				"MEM:\t" + "0\n" +
+				"WB:\t" + "0\n";
+	}
+
+	private String getIFLog() {
+		return pipeline.size() <= INSTRUCTION_FETCH_INDEX ? "0" : getInstructionFetchInstruction().toString();
+	}
+
+	private Instruction getInstructionFetchInstruction() {
+		return pipeline.get(INSTRUCTION_FETCH_INDEX);
+	}
+
+	private String getIDLog() {
+		return pipeline.size() <= INSTRUCTION_DECODE_INDEX ? "0" : getInstructionDecodeInstruction().toString();
+	}
+
+	private Instruction getInstructionDecodeInstruction() {
+		return pipeline.get(INSTRUCTION_DECODE_INDEX);
 	}
 
 	public Instruction getInstruction() {
@@ -21,11 +50,16 @@ public class Pipeline {
 	}
 
 	public Integer countInstructions() {
-		return instructions.size();
+		return originalInstructions.size();
 	}
 
 	public Instruction getCurrentInstruction() {
-		return instructions.isEmpty() ? null : instructions.get(0);
+		return pipeline.isEmpty() ? null : pipeline.get(0);
+	}
+
+	public void advanceCycle() {
+		pipeline.addFirst(originalInstructions.get(currentInstructionIndex));
+		currentInstructionIndex++;
 	}
 
 }
--- PipelineBuilder/src/test/br/ufrn/ioac/model/PipelineTest.java
+++ PipelineBuilder/src/test/br/ufrn/ioac/model/PipelineTest.java
@@ -18,6 +18,7 @@ public class PipelineTest {
 	@Test
 	public void shouldAcceptAnInstruction(){
 		pipeline.add(addInstructionBuilder());
+		pipeline.advanceCycle();
 		assertEquals(pipeline.countInstructions(), Integer.valueOf(1));
 		assertEquals("add $s0, $s1, $s2", pipeline.getCurrentInstruction().toString());
 	}
@@ -30,22 +31,43 @@ public class PipelineTest {
 	}
 
 	@Test
-	public void shouldLogCurentState() {
-		Pipeline pipeline = new Pipeline();
+	public void shouldLogFirstState() {
 		pipeline.add(addInstructionBuilder());
+		pipeline.advanceCycle();
 
 		String logState = "IF:\t" + "add $s0, $s1, $s2\n" +
 				"ID:\t" + "0\n" +
 				"EX:\t" + "0\n" +
 				"MEM:\t" + "0\n" +
 				"WB:\t" + "0\n";
-		assertNotEquals(pipeline.logState(), null);
-		assertNotEquals(pipeline.logState(), "");
-		assertEquals(pipeline.logState(), logState);
+		assertNotEquals(null, pipeline.logState());
+		assertNotEquals("", pipeline.logState());
+		assertEquals(logState, pipeline.logState());
+	}
+
+	@Test
+	public void shouldLogStateWithTwoInstructions(){
+		pipeline.add(addInstructionBuilder());
+		pipeline.add(subInstructionBuilder());
+
+		pipeline.advanceCycle();
+		pipeline.advanceCycle();
+
+		String logState = "IF:\t" + "sub $t0, $t1, $t2\n" +
+				"ID:\t" + "add $s0, $s1, $s2\n" +
+				"EX:\t" + "0\n" +
+				"MEM:\t" + "0\n" +
+				"WB:\t" + "0\n";
+		assertEquals(logState, pipeline.logState());
+
 	}
 
 	private Instruction addInstructionBuilder() {
 		return new Instruction("add","$s0","$s1","$s2");
+	}
+
+	private Instruction subInstructionBuilder() {
+		return new Instruction("sub","$t0","$t1","$t2");
 	}
 
 }
